3d69208db9a602e0d0a46d17ddbb2d9845edb6c9
Adam Roberts <aroberts85@yahoo.com>
2017-08-17 13:53:52 -0400

    create user model

--- api/mix.exs
+++ api/mix.exs
@@ -18,11 +18,10 @@ defmodule Sling.Mixfile do
   #
   # Type `mix help compile.app` for more information.
   def application do
-    [
-      mod: {Sling, []},
-      extra_applications: [:logger]
-    ]
-  end
+  [mod: {Sling, []},
+   applications: [:phoenix, :phoenix_pubsub, :phoenix_html, :cowboy, :logger, :gettext,
+                  :phoenix_ecto, :postgrex, :comeonin]] # :comeonin added here
+end
 
   # Specifies which paths to compile per environment.
   defp elixirc_paths(:test), do: ["lib", "web", "test/support"]
@@ -32,15 +31,16 @@ defmodule Sling.Mixfile do
   #
   # Type `mix help deps` for examples and options.
   defp deps do
-    [
-      {:phoenix, "~> 1.3.0-rc"},
-      {:phoenix_pubsub, "~> 1.0"},
-      {:phoenix_ecto, "~> 3.2"},
-      {:postgrex, ">= 0.0.0"},
-      {:gettext, "~> 0.11"},
-      {:cowboy, "~> 1.0"}
-    ]
-  end
+  [{:phoenix, "~> 1.2.1"},
+   {:phoenix_pubsub, "~> 1.0"},
+   {:phoenix_ecto, "~> 3.0"},
+   {:postgrex, ">= 0.0.0"},
+   {:phoenix_html, "~> 2.6"},
+   {:phoenix_live_reload, "~> 1.0", only: :dev},
+   {:gettext, "~> 0.11"},
+   {:cowboy, "~> 1.0"},
+   {:comeonin, "~> 2.5"}] # :comeonin added here
+end
 
   # Aliases are shortcuts or tasks specific to the current project.
   # For example, to create, migrate and run the seeds file at once:
